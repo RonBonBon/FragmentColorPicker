4d90119e901d1b68762b7d228d2b49a6448d9161
Ron Aricha <ron-aricha@013.net>
2017-05-13 18:36:24 +0300

    Layout Orientation Support

--- app/src/main/java/com/arichafamily/fragmentcolorpicker/MainActivity.java
+++ app/src/main/java/com/arichafamily/fragmentcolorpicker/MainActivity.java
@@ -12,8 +12,18 @@ public class MainActivity extends AppCompatActivity implements Communicator {
     }
 
     @Override
-    public void respond(int data) {
-        FragmentB f2 = (FragmentB) getSupportFragmentManager().findFragmentById(R.id.fragment3);
-        f2.changeColor(data);
+    public void respond(int data)
+    {
+        int displayMode = getResources().getConfiguration().orientation;
+        if (displayMode == 1) // it portrait mode
+        {
+            FragmentB f2 = (FragmentB) getSupportFragmentManager().findFragmentById(R.id.fragment3);
+            f2.changeColor(data);
+        }
+        else // its landscape
+        {
+            FragmentB f2 = (FragmentB) getSupportFragmentManager().findFragmentById(R.id.fragment5);
+            f2.changeColor(data);
+        }
     }
 }
